4b265ef3e9f6a357dfc9eb350e94af729ee76e8d
Moustafa <moustafabayommy@gmail.com>
2021-08-10 15:40:17 +0200

    add mongo bitnami  replica set

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { NorificationsModule } from './norifications/norifications.module';
+import { NotificationsController } from './norifications/notifications.controller';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:dummypass@localhost/notifications')],
-    controllers: [AppController],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/notifications?replicaSet=rs0',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }), NorificationsModule],
+    controllers: [AppController, NotificationsController],
   providers: [AppService],
 })
 export class AppModule {}
